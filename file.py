55f627502bd25e1b268759430ac23becf983e2da
gyuwon-k <gyuwon2202@gmail.com>
2026-05-12 15:44:03 +0900

    0512 15:43

---
+++ file.py
@@ -0,0 +1,11 @@
+import csv
+from scrapper import search_incruit
+
+def save_to_csv(jobs):
+
+	with open("downloads.csv", "w", encoding="cp949") as file:
+		csv_writer = csv.writer(file)
+		csv_writer.writerow(["NO","회사명", "공고명", "지역", "상세보기 페이지"])
+		for i, job in enumerate(jobs):
+			csv_writer.writerow([i+1, job["company"], job["title"], job["location"], job["link"]])
+    
--- scrapper.py
+++ scrapper.py
@@ -1,20 +1,79 @@
 import requests
 from bs4 import BeautifulSoup
 
-keyword = "파이썬"
-url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&page=1"
-response = requests.get(url)
+def search_incruit(keyword, page):
 
-soup = BeautifulSoup(response.text, "html.parser")
-soup.find_all()
-#가상환경 : .\venv\Scripts\activate
-lis = soup.find_all("li", class_="c_col")
-# print(len(lis))
-# print(lis)
 
-for li in lis[0:1]:
-	# company = li.find("a", class_="cpname")
-	# title = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").text.strip()
-	# location = li.find("div", class_="cl_mid").find_all("span")[0].text.strip()
-	link = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").get("href")
-	print(link)
+	for i in range(page+1):
+		page = i * 30
+		url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno=0&page={page}"
+		# print(f"--- 현재 크롤링 중인 URL: {url} ---")
+
+		# print("-" * 50)
+		response = requests.get(url)
+		soup = BeautifulSoup(response.text, "html.parser")
+		# print(soup)
+		# response = requests.get(url)
+		# soup = BeautifulSoup(response.text, "html.parser")
+		# 공고 리스트 추출
+		lis = soup.find_all("li", class_="c_col")
+
+		jobs = []
+		for li in lis:
+			# 각 요소 추출
+			company_tag = li.find("a", class_="cpname")
+			company = company_tag.text.strip() if company_tag else "회사명 없음"
+			
+			cell_mid = li.find("div", class_="cell_mid")
+			title_tag = cell_mid.find("div", class_="cl_top").find("a")
+			title = title_tag.text.strip()
+			link = title_tag.get("href")
+			
+			# 지난번에 확인한 cl_md로 수정 반영
+			cl_md = li.find("div", class_="cl_md")
+			location = cl_md.find_all("span")[0].text.strip() if cl_md else "지역 정보 없음"
+
+			job_data = {
+				"company": company,
+				"title": title,
+				"location": location,
+				"link": link
+			}
+			jobs.append(job_data)
+			# print(f"수집 완료: {title}")
+
+		# print(f"\n[페이지 {i+1} 결과 요약]")
+		# print(jobs)
+		# print("-" * 50)
+
+	return jobs
+
+def search_saramin(keyword, page):
+	
+	for i in range(page+1):
+		page = i
+		response = requests.get("keyword")
+		
+		url = f"https://www.saramin.co.kr/zf_user/search/recruit?searchType=search&keydownAccess=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&searchword={keyword}&panel_type=&search_optional_item=y&search_done=y&panel_count=y&preview=y&recruitPage={page}&recruitSort=relation&recruitPageCount=40&inner_com_type=&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
